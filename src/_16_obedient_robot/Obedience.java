ffff7e96241dac2c949d3971d45bf61dc19f5545
CalebVia <55600946+CalebVia@users.noreply.github.com>
2020-02-13 15:55:41 -0800

    did this

---
+++ src/_16_obedient_robot/Obedience.java
@@ -0,0 +1,10 @@
+package _16_obedient_robot;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Obedience {
+	public static void main(String[] args) {
+		Robot obey = new Robot();
+		
+	}
+}
